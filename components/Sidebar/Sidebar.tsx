891163860963ab127529e1585f157b2be35d8170
Javier Riveros <javiiriveros@hotmail.com>
2021-01-26 18:47:19 -0500

    feat: add sidebar component for showing the filters

---
+++ components/Sidebar/Sidebar.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import "twin.macro";
+
+import { Button } from "@/components/Button";
+import { FilterItem, FilterGroup } from "@/components/Filters";
+
+export const Sidebar = () => {
+  return (
+    <aside tw="col-span-1 md:col-span-3 p-8">
+      <div tw="flex flex-col gap-4">
+        <FilterGroup title="Job type" active={true}>
+          <FilterItem label="Full time jobs" />
+          <FilterItem label="Part time jobs" />
+          <FilterItem label="Remote jobs" />
+          <FilterItem label="Internship jobs" />
+        </FilterGroup>
+
+        <FilterGroup title="Seniority level">
+          <FilterItem label="Mid level" />
+          <FilterItem label="Senior level" />
+        </FilterGroup>
+
+        <FilterGroup title="Salary range">
+          <FilterItem label="$700 - $1000" />
+          <FilterItem label="$1000 - $5000" />
+        </FilterGroup>
+
+        <FilterGroup title="Status">
+          <FilterItem label="Open" />
+          <FilterItem label="Closed" />
+        </FilterGroup>
+
+        <Button as="button">Apply filters</Button>
+      </div>
+    </aside>
+  );
+};
