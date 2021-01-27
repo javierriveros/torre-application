373b6baa992858fd6a5d525192357a5ed18bc04e
Javier Riveros <javiiriveros@hotmail.com>
2021-01-27 10:33:50 -0500

    feat: add button with torre color

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -11,3 +11,5 @@ export const Button = styled.a<ButtonProps>` 
       ? tw`text-gray-600 bg-gray-300 hover:bg-gray-400 hover:text-gray-700 focus:ring-gray-600`
       : tw`text-white bg-blue-600 hover:bg-blue-500 focus:ring-blue-600`}
 `;
+
+export const TorreButton = tw.a`inline-flex text-gray-800 bg-torre-500 items-center justify-center px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-torre-500 focus:ring-offset-2 disabled:opacity-50 hover:opacity-90`;
