bd3b6e278d4b1041fe82d02f7cb23a97ca12c71e
Javier Riveros <javiiriveros@hotmail.com>
2021-01-27 13:35:57 -0500

    feat: add jobs page

--- pages/jobs.tsx
+++ pages/jobs.tsx
@@ -1,42 +1,61 @@
-import * as React from "react";
-import tw from "twin.macro";
-
-import { Layout } from "@/components/Layout";
 import { Button } from "@/components/Button";
-import { JobCard } from "@/components/JobCard";
+import { ConditionalWrap } from "@/components/ConditionalWrap";
+import { Error } from "@/components/Error";
+import { LoadingIcon } from "@/components/Icons/LoadingIcon";
+import { JobCard, JobsSkeleton } from "@/components/Jobs";
+import { Layout } from "@/components/Layout";
 import { Sidebar } from "@/components/Sidebar";
+import { useOpportunities } from "@/hooks/queries";
+import { Opportunity } from "@/services/api";
+import * as React from "react";
+import "twin.macro";
 
 export default function Home() {
+  const [jobCount, setJobCount] = React.useState(20);
+  const { data, isLoading, isError, error, isFetching } = useOpportunities({
+    size: jobCount,
+  });
+
   return (
     <Layout>
       <section tw="w-full bg-gray-50 dark:bg-dark-600 grid grid-cols-1 md:grid-cols-12">
         <Sidebar />
 
         <section tw="col-span-1 md:col-span-9 px-4 pt-8">
-          <h2 tw="text-2xl font-bold dark:text-white">Showing 46 jobs</h2>
+          <h2 tw="text-2xl font-bold dark:text-white">
+            Showing {data?.results?.length} jobs
+          </h2>
           <div tw="grid grid-cols-1 xl:grid-cols-3 2xl:grid-cols-4 gap-8 my-4">
-            {[1, 2, 3, 4, 5, 6].map((l) => (
-              <JobCard
-                key={l}
-                image={"https://github.com/javierriveros.png"}
-                title={"UX /UI Designer"}
-                description={
-                  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed reiciendis saepe totam ut. Assumenda, commodi! Sapiente estdolore nihil repellat possimus error..."
-                }
-                tags={[
-                  { name: "Full time", experience: "potential-to-develop" },
-                  { name: "Full time", experience: "potential-to-develop" },
-                  { name: "Full time", experience: "potential-to-develop" },
-                  { name: "Full time", experience: "potential-to-develop" },
-                  { name: "Full time", experience: "potential-to-develop" },
-                ]}
-                link={"#"}
-              />
+            <Error show={isError || data?.message}>
+              <p>
+                {isError && error.message}
+                {data?.message && data.message}
+              </p>
+            </Error>
+            {isLoading && <JobsSkeleton />}
+            {data?.results?.map((opportunity: Opportunity) => (
+              <JobCard opportunity={opportunity} />
             ))}
           </div>
-          <div tw="mb-8 mt-4 text-center">
-            <Button as={"button"}>Load more</Button>
-          </div>
+          <ConditionalWrap condition={jobCount < 90}>
+            <div tw="mb-8 mt-4 text-center">
+              <Button
+                as={"button"}
+                onClick={() => {
+                  setJobCount(jobCount + 10);
+                }}
+                disabled={isFetching}
+              >
+                {isFetching ? (
+                  <>
+                    <LoadingIcon /> Loading...
+                  </>
+                ) : (
+                  "Load More"
+                )}
+              </Button>
+            </div>
+          </ConditionalWrap>
         </section>
       </section>
     </Layout>
