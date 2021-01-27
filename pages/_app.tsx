5ef908f4a5c6cbf03a3877defd76e89e862b68fb
Javier Riveros <javiiriveros@hotmail.com>
2021-01-27 10:41:49 -0500

    feat: add theme to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,29 @@
 import type { AppProps } from "next/app";
-import GlobalStyles from "@/styles/GlobalStyles";
+import { GlobalStyles } from "twin.macro";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Hydrate } from "react-query/hydration";
+import { ReactQueryDevtools } from "react-query/devtools";
+import Head from "next/head";
+import BaseStyles from "@/styles/BaseStyles";
+import { ThemeProvider } from "@/components/Theme";
+
+const queryClient = new QueryClient();
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <div>
-    <GlobalStyles />
-    <Component {...pageProps} />
-  </div>
+  <QueryClientProvider client={queryClient}>
+    <Hydrate state={pageProps.dehydratedState}>
+      <ThemeProvider initialTheme={"light"}>
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+        </Head>
+        <GlobalStyles />
+        <BaseStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Hydrate>
+    <ReactQueryDevtools />
+  </QueryClientProvider>
 );
 
 export default App;
