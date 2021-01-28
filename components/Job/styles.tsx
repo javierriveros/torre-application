63adc8c15697762deba6e3e586f06a9121aac25c
Javier Riveros <javiiriveros@hotmail.com>
2021-01-28 02:37:58 -0500

    fix: add background cover class to job cover

--- components/Job/styles.tsx
+++ components/Job/styles.tsx
@@ -8,6 +8,6 @@ export const JobInfo = tw.section`col-span-3 bg-white rounded dark:bg-gray-800`;
 export const JobImageCard = tw.div`-mt-16 ml-8 border-4 border-white inline-block rounded-lg bg-white dark:bg-gray-800 dark:border-gray-800`;
 export const JobLocation = tw.span`dark:text-gray-300 text-gray-600 text-sm flex mt-2`;
 
-export const Gradient = tw.div`bg-gradient-to-r from-red-500 to-red-700 w-full h-48 rounded-t-lg`;
+export const Gradient = tw.div`bg-gradient-to-r from-red-500 to-red-700 w-full h-48 rounded-t-lg bg-cover`;
 
 export const SectionTitle = tw.h4`font-medium text-torre-600 dark:text-gray-200`;
